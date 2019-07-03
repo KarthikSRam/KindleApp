cb4d343342a5fe9eb02e482a840adbec6e5f914f
KarthikSRam <karthiksram100@gmail.com>
2019-07-03 12:42:51 -0700

    Added people and Readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Kindle App
+This is a simple Kindle application to rent books from the library
+
+## Running the application
+
+1. Install node and express
+2. Clone repo and run **node server.js**
+3. Go to http://localhost:3000
+4. Go to http://localhost:3000/api/books to get JSON of books
+5. Go to http://localhost:3000/api/people to get JSON of people
--- server.js
+++ server.js
@@ -4,17 +4,36 @@ var express = require('express');
   var books = [
       {
         title: 'Harry Potter',
-        author: 'J.K. Rowling'
+        author: 'J.K. Rowling',
+        quantity: 5,
+        id: 1
       },
       {
         title: 'The Lord of the Rings',
-        author: 'Tolkien'
+        author: 'Tolkien',
+        quantity: 5,
+        id: 2
       },
       {
         title: 'Becoming',
-        author: 'Michelle Obama'
+        author: 'Michelle Obama',
+        quantity: 5,
+        id: 3
       }
     ];
+
+    var people = [
+        {
+          name: 'John',
+          borrowedBook: 'Harry Potter',
+          readingList: ['Becoming']
+        },
+        {
+          name: 'Louis',
+          borrowedBook: 'Becoming',
+          readingList: []
+        }
+    ]
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -24,10 +43,15 @@ var express = require('express');
   app.get('/', function (req, res) {
     //res.send('Welcome to Kindle App!');
     res.sendFile('index.html' , { root : __dirname});
+    //res.render("index", {user: req.user});
   });
 
   app.get('/api/books', function(req, res) {
     res.json(books);
+  });
+
+  app.get('/api/people', function(req, res) {
+    res.json(people);
   });
 
   app.listen(3000, function () {
